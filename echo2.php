e22285a1dee7ee70efdccd40fbf0bbef08f4a6fe
temanuilov <48602750+temanuilov@users.noreply.github.com>
2023-02-03 11:41:01 +0200

    add echo2 update echo

--- echo2.php
+++ echo2.php
@@ -3,10 +3,7 @@
 <?php 
   print "Running on ".$_ENV["WEBSITE_HOSTNAME"]."<br/>";
   print "Headers <br/>";
-  $headers = apache_request_headers();
-  foreach ($headers as $header => $value) {
-      echo "$header: $value <br />\n";
-  };
+  print_r(apache_request_headers());
   print "Request <br/>";
   print_r($_REQUEST);
 ?>
